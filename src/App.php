3657c2be13cb8c9375b0b826551df2c13cc6db60
David Jegat <david.jegat@gmail.com>
2021-05-19 16:39:50 +0200

    Ajout de l'exo - affichage des pages

--- src/App.php
+++ src/App.php
@@ -12,6 +12,18 @@ class App
      */
     static public function start(): void
     {
-        echo 'Bienvenue sur mon blog';
+        // $_GET permet d'accèder au query string
+        // ex: $_GET['page'] retourne la query string "page"
+        // la fonction isset(...) permet de tester si un
+        // élement est présent dans un tableaux.
+
+        $page = 'list';
+
+        // Ici on test si on a envoyé la query "page"
+        if (isset($_GET['page'])) {
+            $page = $_GET['page'];
+        }
+
+        // ici afficher la page avec "require" !
     }
 }
