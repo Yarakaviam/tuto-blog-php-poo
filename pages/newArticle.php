6a8572722b11e4170e6d85e5bdca15d503f0498a
David Jegat <david.jegat@gmail.com>
2021-05-20 11:19:40 +0200

    Ajout du formulauire de creation d'article

--- pages/newArticle.php
+++ pages/newArticle.php
@@ -4,6 +4,22 @@ require __DIR__ . '/partials/themeStart.php';
 
 <h1>Page de création d'un article</h1>
 
+<form method="POST" action="./index.php?page=newArticle">
+    <div class="mb-3">
+        <label for="title" class="form-label">Titre de l'article</label>
+        <input type="text" class="form-control" id="title" name="title">
+    </div>
+    <div class="mb-3">
+        <label for="description" class="form-label">Description de l'article</label>
+        <textarea class="form-control" id="description" name="description"></textarea>
+    </div>
+    <div class="mb-3">
+        <label for="content" class="form-label">Contenue de l'article</label>
+        <textarea class="form-control" id="content" name="content"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Créer</button>
+</form>
+
 <?
 require __DIR__ . '/partials/themeEnd.php';
 ?>
