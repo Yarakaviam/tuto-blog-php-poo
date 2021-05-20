3ee340e4c1ff85c54b4628bacbcd6aeef7974cf5
Yarakaviam <matthieu.delclos@live.fr>
2021-05-20 14:33:29 +0200

    ajout list + newA

--- pages/list.php
+++ pages/list.php
@@ -1,9 +1,31 @@
 <?php
 require __DIR__ . '/partials/themeStart.php';
+
+// On se connecte à la DB
+$pdo = new PDO('mysql:dbname=php-poo-blog;host=localhost', 'root', '');
+
+// On créé une requête SQL
+$sql = 'SELECT * FROM articles';
+// On prépare notre requête SQL
+$request = $pdo->prepare($sql);
+// On exécute la requête
+$request->execute();
+// On récupère les articles
+$articles = $request->fetchAll();
 ?>
 
 <h1>Bienvenue</h1>
 
-<?
+<?php foreach ($articles as $article) : ?>
+    <div class="card my-3">
+        <div class="card-body">
+            <h5 class="card-title"><?= $article['title'] ?></h5>
+            <p class="card-text"><?= $article['description'] ?></p>
+            <a href="./index.php?page=article&id=<?= $article['id'] ?>" class="btn btn-primary">Lire cette article</a>
+        </div>
+    </div>
+<?php endforeach; ?>
+
+<?php
 require __DIR__ . '/partials/themeEnd.php';
 ?>
--- pages/newArticle.php
+++ pages/newArticle.php
@@ -1,9 +1,44 @@
 <?php
 require __DIR__ . '/partials/themeStart.php';
+
+$success = false;
+
+
+if (!empty($_POST)) {
+    // 1 : Connexion à la DB
+    $pdo = new PDO('mysql:dbname=php-poo-blog;host=localhost', 'root', '');
+    var_dump('Connecté !');
+
+    // 2 : Requête de création d'article
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $content = $_POST['content'];
+    var_dump($title, $description, $content);
+
+    // 3 : Création requête SQL, ne pas concaténer les valeurs directement, utiliser des "?"
+    $sql = 'INSERT INTO articles (title, description, content) VALUES (?, ?, ?)';
+
+    // 4 : Préparation de la requête SQL et nous récupéront une requête
+    $request = $pdo->prepare($sql);
+
+    // 5 : Envoyer la requête à la DB. Cela enregistre l'article dans la base.
+    $request->execute([
+        $title,
+        $description,
+        $content,
+    ]);
+
+    $success = true;
+}
 ?>
+
+
 
 <h1>Page de création d'un article</h1>
 
+    <div class="alert alert-success" role="alert">
+        L'article à bien été créé
+    </div>
 <form method="POST" action="./index.php?page=newArticle">
     <div class="mb-3">
         <label for="title" class="form-label">Titre de l'article</label>
@@ -20,6 +55,6 @@ require __DIR__ . '/partials/themeStart.php';
     <button type="submit" class="btn btn-primary">Créer</button>
 </form>
 
-<?
+<?php
 require __DIR__ . '/partials/themeEnd.php';
 ?>
